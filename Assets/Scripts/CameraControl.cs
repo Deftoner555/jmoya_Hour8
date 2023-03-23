dbad5345cd78216b7d01fb4d3f7ebdcc2e4dee7d
Deftoner555 <jmoya555@gmail.com>
2023-03-22 18:48:07 -0700

    started CameraControl

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        if()
+    }
+}
+
